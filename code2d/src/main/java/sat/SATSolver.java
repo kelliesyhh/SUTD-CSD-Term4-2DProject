240246660091c0815752043ae8a198818ceb22f0
aleextw <lee.tze.wei.alex.joel@gmail.com>
2022-03-02 22:14:40 +0800

    Depression

--- code2d/src/main/java/sat/SATSolver.java
+++ code2d/src/main/java/sat/SATSolver.java
@@ -16,9 +16,9 @@ public class SATSolver {
      * unit propagation. The returned environment binds literals of class
      * bool.Variable rather than the special literals used in clausification of
      * class clausal.Literal, so that clients can more readily use it.
-     * 
+     *
      * @return an environment for which the problem evaluates to Bool.TRUE, or
-     *         null if no such environment exists.
+     * null if no such environment exists.
      */
     public static Environment solve(Formula formula) {
         System.out.println("SAT solver starts!!!");
@@ -26,52 +26,53 @@ public class SATSolver {
         Environment environment = new Environment();
 
         ImList<Clause> clauses = formula.getClauses();
-        solve(clauses, environment);
 
-        return environment;
+        return solve(clauses, environment);
     }
 
     /**
      * Takes a partial assignment of variables to values, and recursively
      * searches for a complete satisfying assignment.
-     * 
-     * @param clauses
-     *            formula in conjunctive normal form
-     * @param env
-     *            assignment of some or all variables in clauses to true or
-     *            false values.
+     *
+     * @param clauses formula in conjunctive normal form
+     * @param env     assignment of some or all variables in clauses to true or
+     *                false values.
      * @return an environment for which all the clauses evaluate to Bool.TRUE,
-     *         or null if no such environment exists.
+     * or null if no such environment exists.
      */
     private static Environment solve(ImList<Clause> clauses, Environment env) {
-        if (clauses.isEmpty()){
+        if (clauses.isEmpty()) {
             return env;
         } else {
+            if (clauses.first().isEmpty()) return null;
+
             //check if got empty clause in list of clauses
             Clause shortestClause = clauses.first();
-            for (Clause clause : clauses){
-                if (clause.isEmpty()){
+
+            for (Clause clause : clauses) {
+                if (clause.isEmpty()) {
                     return null;
-                } else { //shun bian identify the shortest clause for the recursive part later
-                    if (clause.size() < shortestClause.size()){
+                } else if (clause.size() < shortestClause.size()) {
                         shortestClause = clause;
-                    }
                 }
             }
-            //the recursive code starts here
+
+            // Pick any literal l and set it to true
             Literal l = shortestClause.chooseLiteral();
             env = env.putTrue(l.getVariable());
-            clauses = SATSolver.substitute(clauses,l);
-            Environment result = solve(clauses, env);
-            if (result == null && shortestClause.size() > 1){
+
+            // Evaluate the remainder of the clauses given that l is true
+            Environment result = solve(SATSolver.substitute(clauses, l), env);
+
+            if (result == null && shortestClause.size() > 1) {
+                // Set l to false and return whatever solution we get
                 env = env.putFalse(l.getVariable());
-                clauses = SATSolver.substitute(clauses,l);
-                return solve(clauses, env);
-            } else {
-                return result;
+                result = solve(SATSolver.substitute(clauses, l.getNegation()), env);
             }
+            return result;
         }
     }
+
 
     /**
      * given a clause list and literal, produce a new list resulting from
@@ -85,18 +86,18 @@ public class SATSolver {
      */
     private static ImList<Clause> substitute(ImList<Clause> clauses,
             Literal l) {
-        // for each clause in list, check if it contains literal
-        // delete clause if literal be found
+
         ImList<Clause> newClauses = new EmptyImList<>();
+//        System.out.println(l);
+//        System.out.println(clauses);
+        // If the clause doesn't reduce to null (true), add it to our new clause list
         for (Clause clause : clauses) {
-            if (clause.contains(l)){
-                if (clause.reduce(l) != null) {
-                    newClauses = newClauses.add(clause.reduce(l));
-                }
-            } else {
-                newClauses = newClauses.add(clause);
+            if (clause.reduce(l) != null) {
+                newClauses = newClauses.add(clause.reduce(l));
             }
         }
+//        System.out.println(newClauses);
+//        System.out.println();
         return newClauses;
     }
 
--- code2d/src/main/java/sat/SATSolverTest.java
+++ code2d/src/main/java/sat/SATSolverTest.java
@@ -44,116 +44,9 @@ public class SATSolverTest {
 
     // https://people.sc.fsu.edu/~jburkardt/data/cnf/cnf.html
 
-
-    // TODO: add the main method that reads the .cnf file and calls SATSolver.solve to determine the satisfiability
-//    public static void main(String[] args) {
-////        String filepath = args[0];
-//        String filepath = "/Users/kellie/Documents/50.001/2DProject/code2d/src/main/java/sat/s8Sat.cnf";
-//        System.out.println(filepath);
-//
-////        // include this part for the first time you run the cnf, to obtain the desired output (just check if it's working)
-////        PrintStream out = null;
-////        try {
-////            out = new PrintStream(new FileOutputStream("/Users/kellie/Documents/50.001/2DProject/code2d/src/main/java/sat/cnf-output.txt"));
-////        } catch (FileNotFoundException e) {
-////            e.printStackTrace();
-////        };
-//
-//        try {
-//            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(filepath));
-//            int data = bufferedInputStream.read();
-//            ImList<Literal> literalImList = new EmptyImList<>();
-//            ImList<Clause> clauseImList = new EmptyImList<>();
-//            // if data is -1, that means that it's the end of the file
-//            while (data != -1) {
-////                System.out.println((char) data + ": " + String.valueOf(data));
-//                data = bufferedInputStream.read();
-//
-////                // include this part for the first time you run the cnf, to obtain the desired output (just check if it's working)
-////                String output = (char) data + ": " + data + "\n";
-////                out.print(output);
-//
-//                // if data is 32 (ASCII), it means a space
-//                // if data is 10 (ASCII), it means a line break (\n)
-//                // if data is 48 (ASCII), it means a 0.  clause is terminated by '0'
-//                // if data is 99 (ASCII), it means c. c in cnf refers to comment lines
-//                // if data is 112 (ASCII), it means p. p in cnf refers to problem lines
-//
-//                /* pseudocode:
-//                we will first detect the presence of a line break.
-//                if a line break is present, we will track the next thing that occurs right after the line break (i.e. p? or some other number)
-//                if the next thing is a p, we carry on reading. else, we start to process it into the formula
-//                */
-//                while (data != 48) {
-//                    data = bufferedInputStream.read();
-//                    if (data == 99 || data == 112) {
-//                        while (data != 32) {
-//                            data = bufferedInputStream.read();
-//                        }
-//                    }
-//                    else if (data != 99 && data != 112) {
-//                        StringBuilder temp = new StringBuilder();
-//                        // if data is 32, it means a space (signifies OR)
-//                        // while you haven't reached a space, keep reading (eg: -16, -2, -55)
-//                        while (data != 32) {
-//                            temp.append((char) (data));
-//                            data = bufferedInputStream.read();
-//                        }
-//                        if (data == 32) {
-//                            data = bufferedInputStream.read();
-//                            // if data is 45, it means '-' aka a negative literal.
-//                            System.out.println((char) data + ": " + String.valueOf(data));
-//                            if (data == 45) {
-//                                System.out.println("-" + temp.toString());
-//                                Variable variable = new Variable(temp.toString());
-//                                Literal negLiteral = NegLiteral.make(variable);
-//                                literalImList.add(negLiteral);
-//                            } else if (data >= 49 && data <= 57) {
-//                                System.out.println(temp.toString());
-//                                Variable variable = new Variable(temp.toString());
-//                                Literal posLiteral = PosLiteral.make(variable);
-//                                literalImList.add(posLiteral);
-//                            }
-//                        }
-//                    }
-//                }
-//
-//
-//                if (data == 48) {
-//                    int i = 0;
-//                    Literal[] literals = new Literal[literalImList.size()];
-//                    while (literalImList.iterator().hasNext()) {
-//                        literals[i] = literalImList.iterator().next();
-//                        i += 1;
-//
-//                        System.out.println("LITERAL");
-//                        System.out.println(literalImList.iterator().next());
-//                    }
-//
-//                    clauseImList.add(makeCl(literals));
-//                }
-//            }
-//            int i = 0;
-//            Clause[] clauses = new Clause[clauseImList.size()];
-//            while (clauseImList.iterator().hasNext()) {
-//                clauses[i] = clauseImList.iterator().next();
-//                i += 1;
-//
-//                System.out.println("CLAUSE");
-//                System.out.println(clauseImList.iterator().next());
-//            }
-//            Formula formula = makeFm(clauses);
-////            SATSolver.solve(formula);
-//
-//            }
-//        catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
     public static void main(String[] args) {
 //        String filepath = args[0];
-        String filepath = "C:\\Users\\Razer\\OneDrive - Singapore University of Technology and Design\\SUTD\\Y1\\Term 4\\50.002 Computation Structures\\2D\\50001_Project-2D-starting\\sampleCNF\\largeUnsat.cnf";
+        String filepath = "C:\\Users\\Razer\\OneDrive - Singapore University of Technology and Design\\SUTD\\Y1\\Term 4\\50.002 Computation Structures\\2D\\50001_Project-2D-starting\\sampleCNF\\RCAout.cnf";
 
         try {
             InputStream inputStream = new FileInputStream(filepath);
@@ -203,8 +96,12 @@ public class SATSolverTest {
             }
 
             Clause[] temp = clauses.toArray(new Clause[0]); // Convert to standard array to use makeFm
-            Formula fm = makeFm(temp);
 
+            reader.close();
+            clauses = null;
+            literals = null;
+
+            Formula fm = makeFm(temp);
             long started = System.nanoTime();
             Environment e = SATSolver.solve(fm);
             long time = System.nanoTime();
